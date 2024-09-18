4d1255f77f25ab9417c7167b93c3062fc06cf9b9
Kristin Bäßler <kristin.baessler@iml.fraunhofer.de>
2024-09-18 07:03:32 +0000

    Bug: Connection Error

---
+++ apps/frontend/src/environments/environment.prod.ts
@@ -0,0 +1,20 @@
+const BASE_URL = 'https://bff.affds.apps.blockchain-europe.iml.fraunhofer.de';
+
+export const environment = {
+  production: true,
+  OFFERS: {
+    URL: `${BASE_URL}/offers`,
+    ACCEPT: '/accept',
+    DECLINE: `${BASE_URL}/offers/decline`
+  },
+  ORDERS: {
+    URL: `${BASE_URL}/orders`,
+    ORDER: '/orders'
+  },
+  PRODUCTS: {
+    URL: `${BASE_URL}/products`,
+  },
+  TRADERECEIVABLES: {
+    URL: `${BASE_URL}/trade-receivables`,
+  },
+};
