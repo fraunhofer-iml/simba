9ea94fe138ae580138b37190cb1bc131a278c0cf
Dominik Sparer <dominik.sparer@iml.fraunhofer.de>
2024-09-18 07:43:25 +0000

    Bugfix/pipeline2

--- prisma/migrations/20240912071238_v0_0_1/migration.sql
+++ prisma/migrations/20240912071238_v0_0_1/migration.sql
@@ -1,4 +1,24 @@
 -- CreateTable
+CREATE TABLE "Offer" (
+    "id" TEXT NOT NULL,
+    "creationDate" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "price" DOUBLE PRECISION NOT NULL,
+    "status" TEXT NOT NULL,
+    "orderId" TEXT NOT NULL,
+
+    CONSTRAINT "Offer_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "variant" TEXT NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "Machine" (
     "id" TEXT NOT NULL,
     "description" TEXT NOT NULL,
@@ -44,6 +64,7 @@ CREATE TABLE "TradeReceivable" (
     "creationDate" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "value" DOUBLE PRECISION NOT NULL,
     "debitorId" TEXT NOT NULL,
+    "orderId" TEXT NOT NULL,
     "invoiceId" TEXT NOT NULL,
 
     CONSTRAINT "TradeReceivable_pkey" PRIMARY KEY ("id")
@@ -56,16 +77,27 @@ CREATE TABLE "Order" (
     "completionDate" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
     "amount" INTEGER NOT NULL,
     "status" TEXT NOT NULL,
-    "calendarWeek" TEXT NOT NULL,
-    "offers" TEXT[],
+    "dueMonth" TEXT NOT NULL,
     "machines" TEXT[],
+    "acceptedByOrderId" TEXT,
     "productId" TEXT NOT NULL,
     "participantId" TEXT NOT NULL,
-    "offerId" TEXT,
     "tradeReceivableId" TEXT,
 
     CONSTRAINT "Order_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "TradeReceivable_orderId_key" ON "TradeReceivable"("orderId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Order_acceptedByOrderId_key" ON "Order"("acceptedByOrderId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Order_tradeReceivableId_key" ON "Order"("tradeReceivableId");
+
+-- AddForeignKey
+ALTER TABLE "Offer" ADD CONSTRAINT "Offer_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "Order"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Invoice" ADD CONSTRAINT "Invoice_debitorId_fkey" FOREIGN KEY ("debitorId") REFERENCES "Participant"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -74,16 +106,16 @@ ALTER TABLE "Invoice" ADD CONSTRAINT "Invoice_debitorId_fkey" FOREIGN KEY ("debi
 ALTER TABLE "TradeReceivable" ADD CONSTRAINT "TradeReceivable_debitorId_fkey" FOREIGN KEY ("debitorId") REFERENCES "Participant"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
+ALTER TABLE "TradeReceivable" ADD CONSTRAINT "TradeReceivable_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "Order"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE "TradeReceivable" ADD CONSTRAINT "TradeReceivable_invoiceId_fkey" FOREIGN KEY ("invoiceId") REFERENCES "Invoice"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Order" ADD CONSTRAINT "Order_acceptedByOrderId_fkey" FOREIGN KEY ("acceptedByOrderId") REFERENCES "Offer"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Order" ADD CONSTRAINT "Order_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Order" ADD CONSTRAINT "Order_participantId_fkey" FOREIGN KEY ("participantId") REFERENCES "Participant"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Order" ADD CONSTRAINT "Order_offerId_fkey" FOREIGN KEY ("offerId") REFERENCES "Offer"("id") ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Order" ADD CONSTRAINT "Order_tradeReceivableId_fkey" FOREIGN KEY ("tradeReceivableId") REFERENCES "TradeReceivable"("id") ON DELETE SET NULL ON UPDATE CASCADE;
