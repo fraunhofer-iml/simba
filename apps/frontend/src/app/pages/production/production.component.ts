889f993e84b273e93b34ff6597c1b557aa730a24
Kristin Bäßler <kristin.baessler@iml.fraunhofer.de>
2025-02-21 13:28:44 +0000

    AFFDS-140: Add url for streaming page

--- apps/frontend/src/app/pages/production/production.component.ts
+++ apps/frontend/src/app/pages/production/production.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-production',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './production.component.scss'
 })
 export class ProductionComponent {
+  streamUrl: SafeResourceUrl;
 
+  constructor(private readonly sanitizer: DomSanitizer) {
+    // NOSONAR: The source is only available over HTTP
+    this.streamUrl = this.sanitizer.bypassSecurityTrustResourceUrl('http://unity.flw.mb.tu-dortmund.de');
+  }
 }
